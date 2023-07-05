ad13652af1e2ad49ec270c24ddd6b555b3fabda7
RadCzu <radekczuj@gmail.com>
2023-07-05 17:26:39 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = "welcome to the new blog";
+  const likes = 50;
+  //const person = {name: "yoshi", age: 30}
+  const link = "https://www.google.com/";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random()*10}</p>
+        <a href = {link}>Twoja stara</a>
       </div>
     </div>
   );
