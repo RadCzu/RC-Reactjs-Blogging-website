2383773d870e03ee5f42f1e87b32ea3c2adb5eb4
RadCzu <radekczuj@gmail.com>
2023-07-07 16:59:45 +0200

    button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.Log("twoja stara");
+  }
   return ( 
     <div className = "Home">
       <h2>Homepage</h2>
+      <button onClick = {handleClick}>Click me</button>
     </div>
    );
 }
