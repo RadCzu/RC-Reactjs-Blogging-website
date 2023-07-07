1cdd5a374e9ebf12d921da8fbbd9d29668671ef5
RadCzu <radekczuj@gmail.com>
2023-07-07 17:11:28 +0200

    lesson 7 + mario

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,28 @@
 const Home = () => {
 
-  const handleClick = () => {
-    console.Log("twoja stara");
+  let name = "mario";
+
+  const handleClick = (e) => {
+    //property: event, jest automatyczne
+    console.log("twoja stara", e);
+    name = "luigi";
   }
+
+  const handleClickAgain = (name, e) => {
+    console.log(`${name}, umarł na ligmę`, e);
+  }
+
   return ( 
     <div className = "Home">
       <h2>Homepage</h2>
       <button onClick = {handleClick}>Click me</button>
+      <p>{name}</p>
+      <button onClick = {
+        (e) => {
+          // ta funkcja z parametrami musi byc anonimowa, inaczej jest wywolana raz na starcie
+          handleClickAgain("yoshi", e);
+        }
+        }>Click me again</button>
     </div>
    );
 }
