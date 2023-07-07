9423002cd806e6671f12ea92df1c9b925e411980
RadCzu <radekczuj@gmail.com>
2023-07-07 17:28:43 +0200

    home.js

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,29 @@
+import { useState } from "react";
+
 const Home = () => {
 
-  let name = "mario";
+  //tzw "hook"
+  const[name, setName] = useState("Mario");
+  const[age, setAge] = useState(50);
 
   const handleClick = (e) => {
     //property: event, jest automatyczne
     console.log("twoja stara", e);
-    name = "luigi";
+    setName("Luigi");
+    setAge(age + 1);
   }
 
   const handleClickAgain = (name, e) => {
     console.log(`${name}, umarł na ligmę`, e);
+    setName("Mario");
+    setAge(age + 1);
   }
 
   return ( 
     <div className = "Home">
       <h2>Homepage</h2>
       <button onClick = {handleClick}>Click me</button>
-      <p>{name}</p>
+      <p>{`${name} is ${age} years old`}</p>
       <button onClick = {
         (e) => {
           // ta funkcja z parametrami musi byc anonimowa, inaczej jest wywolana raz na starcie
